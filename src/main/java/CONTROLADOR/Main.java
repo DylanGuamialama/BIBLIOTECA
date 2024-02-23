c8b76e5cf4e313a86dc3b5f4726a5449602138a4
DylanGuamialama <dylan.guamialama354@ist17dejulio.edu.ec>
2024-02-23 13:50:58 -0500

    Desarrollo 2

--- src/main/java/CONTROLADOR/Main.java
+++ src/main/java/CONTROLADOR/Main.java
@@ -1,16 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.mycompany.biblioteca;
+package CONTROLADOR;
 
 /**
  *
  * @author guami
  */
-public class BIBLIOTECA {
-
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MENU p = new MENU();
+        p.setVisible(true);
     }
 }
